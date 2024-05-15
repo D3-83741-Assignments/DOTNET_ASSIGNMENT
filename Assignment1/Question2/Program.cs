05785f29d33ad6f802f278141dd45f81f954173d
Avadhut1212 <avadhutsakhare1212@gmail.com>
2024-05-15 18:21:33 +0530

    Assignment1

---
+++ Assignment1/Question2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a = Convert.ToInt32(args[0]);
+            int b = Convert.ToInt32(args[1]);
+            Console.WriteLine(" 0-Exit\n 1-Add\n 2-Sub\n 3-Mul\n 4-Div ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Thank u!");
+                    break;
+
+                case 1:
+
+                    Console.WriteLine("Addition is :"+(a + b));
+                    break;
+                case 2:
+                    Console.WriteLine("Substraction is :" + (a - b));
+                    break;
+                case 3:
+                    Console.WriteLine("Multiplication is :"+ (a * b));
+                    break;
+                case 4:
+                    Console.WriteLine("Division is :" +(a / b));
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment1/Question3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int choice = -1;
+
+            do
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1.Addtion");
+                Console.WriteLine("2.Substraction");
+
+                Console.WriteLine("3.Multiplication");
+                Console.WriteLine("4.Division");
+
+
+
+                Console.WriteLine("enter the choice");
+
+                string x = Console.ReadLine();
+                choice = Convert.ToInt32(x);
+                int res = 0;
+                int num1 = 0, num2 = 0;
+                if (choice > 0)
+                {
+                    Console.WriteLine("enter the 1st number ");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+
+                    Console.WriteLine("enter the 2nd number ");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("thank u!");
+                        break;
+                    case 1:
+                        res = num1 + num2;
+                        Console.WriteLine( "Addition is :"+res);
+                        break;
+
+                    case 2:
+                        res = num1 - num2;
+                        Console.WriteLine("Substarction is :" + res);
+                        break;
+
+                    case 3:
+
+                        res = num1 * num2;
+                        Console.WriteLine("Multiplication is :" + res);
+                        break;
+
+                    case 4:
+                        res = num1 / num2;
+                        Console.WriteLine("Multiplication is :" + res);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+
+            } while (choice != 0);
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
